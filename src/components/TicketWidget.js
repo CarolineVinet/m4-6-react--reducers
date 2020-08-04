2a7ede64b1764a081e4f9a4977b6b305952ce513
Caroline Vinet <carovinet@gmail.com>
2020-08-04 14:45:25 -0400

    exercise4

--- src/components/TicketWidget.js
+++ src/components/TicketWidget.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-import { getRowName, getSeatNum } from '../helpers';
-import { range } from '../utils';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import seatimage from "../assets/seat-available.svg";
+import { getRowName, getSeatNum } from "../helpers";
+import { range } from "../utils";
+import { SeatContext } from "./SeatContext";
 
 const TicketWidget = () => {
-  // TODO: use values from Context
-  const numOfRows = 6;
-  const seatsPerRow = 6;
+  const { state } = useContext(SeatContext);
 
-  // TODO: implement the loading spinner <CircularProgress />
-  // with the hasLoaded flag
+  const numOfRows = state.numOfRows;
+  const seatsPerRow = state.seatsPerRow;
+
+  if (state.hasloaded === false) {
+    return <CircularProgress />;
+  }
 
   return (
     <Wrapper>
-      {range(numOfRows).map(rowIndex => {
+      {range(numOfRows).map((rowIndex) => {
         const rowName = getRowName(rowIndex);
 
         return (
           <Row key={rowIndex}>
             <RowLabel>Row {rowName}</RowLabel>
-            {range(seatsPerRow).map(seatIndex => {
+            {range(seatsPerRow).map((seatIndex) => {
               const seatId = `${rowName}-${getSeatNum(seatIndex)}`;
 
               return (
                 <SeatWrapper key={seatId}>
-                  {/* TODO: Render the actual <Seat /> */}
+                  <SeatImage src={seatimage}></SeatImage>
                 </SeatWrapper>
               );
             })}
@@ -60,5 +63,7 @@ const RowLabel = styled.div`
 const SeatWrapper = styled.div`
   padding: 5px;
 `;
+
+const SeatImage = styled.img``;
 
 export default TicketWidget;
