c884f3013488c6f5c973892fcb273dfd6b82cbbb
Caroline Vinet <carovinet@gmail.com>
2020-08-04 13:36:10 -0400

    exercise 2

---
+++ src/components/SeatContext.js
@@ -0,0 +1,34 @@
+export const SeatContext = React.createContext();
+
+const initialState = {
+  hasloaded: false,
+  seats: null,
+  numOfRows: 0,
+  seatsPerRow: 0,
+};
+
+function reducer(state, action) {}
+
+export const SeatProvider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const receiveSeatInfoFromServer = (data) => {
+    dispatch({
+      type: "receive-seat-info-from-server",
+      ...data,
+    });
+  };
+
+  return (
+    <SeatContext.Provider
+      value={{
+        state,
+        actions: {
+          receiveSeatInfoFromServer,
+        },
+      }}
+    >
+      {children}
+    </SeatContext.Provider>
+  );
+};
